1292d34f4550d81febd7c4e34160a16e5580d0cd
Laurynas Leišis <lauris.leisis@gmail.com>
2019-12-20 11:08:03 +0200

    :|

--- js/main.js
+++
@@ -1,2 +0,0 @@
-require('web3');
-var web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
--- src/js/app.js
+++ src/js/app.js
@@ -66,53 +66,77 @@ App = {
 
         var acc =web3.eth.accounts;
         acc = acc[0];
-          App.contracts.Housing.deployed().then(function(data) {
-             var instance = data;
-             console.log("1");
-             return instance.setBuyer(acc);
-           });
-          App.contracts.Housing.deployed().then(function(data2) {
-           var instance1 = data2;
-             console.log("2");
+          // App.contracts.Housing.deployed().then(function(data) {
+          //    var instance = data;
+          //    console.log("1");
+          //    return instance.setBuyer(acc);
+          //  });
+          // App.contracts.Housing.deployed().then(function(data2) {
+          //  var instance1 = data2;
+          //    console.log("2");
 
-            return instance1.setRegistor(registor);
-           });
+          //   return instance1.setRegistor(registor);
+          //  });
           App.contracts.Housing.deployed().then(function(data3){
             var ownershipInstance = data3;
              console.log("3");
 
             return ownershipInstance.sendOwnershipInquiry(house_number);
            });
-           // App.contracts.Housing.deployed().then(function(data4){
-           //  var send1 = data4;     
-           //   console.log("4");
+          $("#sendPrice").on("click", function(){
+             App.contracts.Housing.deployed().then(function(data4){
+              var send1 = data4;     
+               console.log("4");
 
-           //       return send1.sendPrice(parseInt(house_number), parseInt(price), 1);                  
-           // });
-           // App.contracts.Housing.deployed().then(function(data5) {
-           //  var send2 =data5;
-           //   console.log("5");
+                   return send1.sendPrice(parseInt(house_number), parseInt(price), 1);                  
+             });
+             App.contracts.Housing.deployed().then(function(data5) {
+              var send2 =data5;
+               console.log("5");
 
-           //   return send2.sendPrice(house_number, 0.5 , 2);
-           // });
-           //  App.contracts.Housing.deployed().then(function(data6){
-           //  var send3 =data6;
-           //   console.log("6");
+               return send2.sendPrice(house_number, 0.5 , 2);
+             });
+          });
+          $("#safePay").on("click", function(){
 
-           //    return send3.sendSafepay(house_number);
-           // });
-           //   App.contracts.Housing.deployed().then(function(data7){
-           //   var send4 =data7;
-           //   return send4.sendInvoice(house_number, "2019");
-           // });
-           //   App.contracts.Housing.deployed().then(function(data8) {   
-           //   var send5 =data8;
-           //   return send5.delivery(house_number, "2019");
-           // });
+            App.contracts.Housing.deployed().then(function(data6){
+            var send3 =data6;
+             console.log(house_number);
+
+              return send3.sendSafepay(house_number);
+           });
+          })
+          $("#sendInvoice").on("click", function(){
+
+             App.contracts.Housing.deployed().then(function(data7){
+             var send4 =data7;
+             return send4.sendInvoice(house_number, "2019");
+           });
+          })
+          $("#deliver").on("click", function(){
+
+             App.contracts.Housing.deployed().then(function(data8) {   
+             var send5 =data8;
+             return send5.delivery(house_number, "2019");
+           });
+          })
         });
 
       
       
+    },
+    setUsers: function(){
+      App.contracts.Housing.deployed().then(function(data) {
+             var instance = data;
+             console.log("1");
+             return instance.setBuyer("0x86406cB56D2502c7F8f98F0Be98ea7deBD6bE200");
+           });
+          App.contracts.Housing.deployed().then(function(data2) {
+           var instance1 = data2;
+             console.log("2");
+
+            return instance1.setRegistor("0x0ab75c54c7c053f6cf226a26e603404c0c9fab3d");
+           });
     },
     sendPrice: function(){
       App.contracts.Housing.deployed().then(function(data4){
